028206d0f72d22a123d9437225cab7c915bf17ed
ManOfGod14 <maliki.tcherou1@gmail.com>
2023-07-28 21:52:56 +0100

    Initial commit

---
+++ 0-prep-week/demo/script.js
@@ -0,0 +1,9 @@
+const pi = 3.14;
+var val1 = "Bonjour";
+let val2 = 100;
+
+let h1Elt = document.querySelector('h1');
+h1Elt.innerHTML = "Bonjour tout le monde ("+pi+")";
+
+// console.log("Bonjour ssi rachid")
+// console.error("Bonjour ssi rachid")
