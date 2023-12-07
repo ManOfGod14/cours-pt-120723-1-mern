3bdb022dd322c5f98d3cfdad4ee9a5e07be3c951
ManOfGod14 <maliki.tcherou1@gmail.com>
2023-12-07 10:15:25 +0100

    updated 20231207

--- 3-javascript/challenges/day-18/assets/js/app.js
+++ 3-javascript/challenges/day-18/assets/js/app.js
@@ -83,7 +83,7 @@ const products = [
 ];
 
 // const testElt = document.createElement('div');
-// testElt.classList.add("col.mb-5");
+// testElt.classList.add("col", "mb-5");
 // testElt.innerHTML = "<span>Hello</span>";
 // console.log(testElt);
 
@@ -93,5 +93,76 @@ function generateProductList() {
     
     products.forEach(product => {
         const productItem = document.createElement('div');
+        productItem.classList.add('col', 'mb-5');
+        productItem.innerHTML = `
+            <div class="card h-100">
+                ${product.hasSaleBadge ? `<div class="badge bg-dark text-white position-absolute" style="top: 0.5rem; right: 0.5rem;">Sale</div>` : ``}
+
+                <img class="card-img-top" src="${product.img}" alt="">
+
+                <div class="card-body p-4">
+                    <div class="text-center">
+                        <h5 class="fw-bolder">${product.name}</h5>
+
+                        <div class="d-flex justify-content-center text-warning mb-2">
+                            ${productNoteStars(product.note)}
+                        </div>
+
+                        ${product.discount ? `<span class="text-muted text-decoration-line-through">${formatPrice(product.price)}</span>` : ``}
+                        <span>${formatPrice(calculateDiscountedPrice(product.price, product.discount))}</span>
+                    </div>
+                </div>
+
+                ${product.hasCartBtn ?
+                `<div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
+                    <div class="text-center">
+                        <a class="btn btn-outline-dark btn-sm mt-auto" href="#">
+                            Add to cart
+                        </a>
+                    </div>
+                </div>`
+                : `` }
+            </div>
+        `;
+        productListContainer.appendChild(productItem);
     });
 }
+
+// appel de fonction
+generateProductList();
+
+// formatage du prix avec la devise
+function formatPrice(price) {
+    return price.toLocaleString('fr-FR', {style:'currency', currency: 'MAD'});
+}
+
+// fonction pour caculer la remise en pourcentage (le prix remisé)
+function calculateDiscountedPrice(price, discount) {
+    if(discount !== null && discount > 0 && discount < 100) {
+        const discountAmount = price * (discount / 100);
+        return price - discountAmount;
+    } else {
+        return price;
+    }
+}
+
+// product note starts
+// soit note = 5, ou 4, ... 1, 0
+// on sait qu'on a aux max 5 etoiles
+// <div class="bi bi-star-fill"></div>
+// <div class="bi bi-star"></div>
+function productNoteStars(note) {
+    let divStars = ``;
+    const emptyStars = 5 - note; // 5 - 2 = 3
+
+    for(let i = 0; i < note; i++) {
+        divStars += `<div class="bi bi-star-fill"></div>`;
+    }
+
+    for(let j = 0; j < emptyStars; j++) {
+        divStars += `<div class="bi bi-star"></div>`;
+    }
+    
+    return divStars;
+}
+
