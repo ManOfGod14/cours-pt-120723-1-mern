4e0c08c05c32d02c8f6e548b7ac558209e6e19fc
ManOfGod14 <maliki.tcherou1@gmail.com>
2024-02-07 21:04:29 +0100

    updated 20240207 2104

--- 3-javascript/tps/tp-generique-typescript/app.ts
+++ 3-javascript/tps/tp-generique-typescript/app.ts
@@ -71,4 +71,60 @@ const tableauFiltre: number[] = filtrerTableau(
   (x) => x % 2 === 0
 );
 
-console.log(tableauFiltre);
+// console.log(tableauFiltre);
+
+/**
+ * Ex06
+ */
+class Empileur<T> {
+  private pile: T[] = [];
+
+  empiler(element: T): void {
+    this.pile.push(element);
+  }
+
+  depiler(): T | undefined {
+    return this.pile.pop();
+  }
+}
+
+// tester la classe Empileur
+// instancier la classe Empileur
+const empileurEntiers = new Empileur<number>();
+empileurEntiers.empiler(8);
+empileurEntiers.empiler(4);
+empileurEntiers.empiler(7);
+// console.log(empileurEntiers);
+
+const sommet: number | undefined = empileurEntiers.depiler();
+// console.log(sommet);
+
+/**
+ * Ex07
+ * {"un": 1, "deux": 2}
+ */
+class Dictionnaire<Cle, Valeur> {
+  private entries: Map<Cle, Valeur> = new Map();
+
+  ajouter(key: Cle, value: Valeur): void {
+    this.entries.set(key, value);
+  }
+
+  recuperer(key: Cle): Valeur | undefined {
+    return this.entries.get(key);
+  }
+
+  supprimer(key: Cle): void {
+    this.entries.delete(key);
+  }
+}
+
+// tester la classe Dictionnaire
+const dic1 = new Dictionnaire<string, number>();
+dic1.ajouter("un", 1);
+dic1.ajouter("deux", 2);
+dic1.ajouter("trois", 3);
+console.log(dic1);
+
+const valDeux: number | undefined = dic1.recuperer("deux");
+console.log(valDeux);
--- 3-javascript/tps/tp-generique-typescript/dist/app.js
+++ 3-javascript/tps/tp-generique-typescript/dist/app.js
@@ -1,13 +1,4 @@
 "use strict";
-var __spreadArray = (this && this.__spreadArray) || function (to, from, pack) {
-    if (pack || arguments.length === 2) for (var i = 0, l = from.length, ar; i < l; i++) {
-        if (ar || !(i in from)) {
-            if (!ar) ar = Array.prototype.slice.call(from, 0, i);
-            ar[i] = from[i];
-        }
-    }
-    return to.concat(ar || Array.prototype.slice.call(from));
-};
 /**
  * Ex0
  */
@@ -21,24 +12,24 @@ function addition(a, b) {
 function identite(valeur) {
     return valeur;
 }
-var resIdentite1 = identite(45);
-var resIdentite2 = identite("Hello");
+const resIdentite1 = identite(45);
+const resIdentite2 = identite("Hello");
 /**
  * Ex02
  */
 function creerPaire(valeur1, valeur2) {
     return [valeur1, valeur2];
 }
-var maPaire1 = creerPaire(9, 5);
-var maPaire2 = creerPaire(9, "Hello");
+const maPaire1 = creerPaire(9, 5);
+const maPaire2 = creerPaire(9, "Hello");
 /**
  * Ex03
  */
 function longueurTableau(tableau) {
     return tableau.length;
 }
-var long1 = longueurTableau([5, 4, 1, 9, 9]);
-var long2 = longueurTableau([
+const long1 = longueurTableau([5, 4, 1, 9, 9]);
+const long2 = longueurTableau([
     "Hello",
     4,
     1,
@@ -53,10 +44,10 @@ var long2 = longueurTableau([
  * Ex04
  */
 function fusionnerTableaux(tableau1, tableau2) {
-    return __spreadArray(__spreadArray([], tableau1, true), tableau2, true);
+    return [...tableau1, ...tableau2];
 }
-var tabFusion1 = fusionnerTableaux(["a", "c", "d"], ["b", "f"]);
-var tabFusion2 = fusionnerTableaux([6, 8, 1, 4], [5, 9, 7]);
+const tabFusion1 = fusionnerTableaux(["a", "c", "d"], ["b", "f"]);
+const tabFusion2 = fusionnerTableaux([6, 8, 1, 4], [5, 9, 7]);
 /**
  * Ex05
  */
@@ -64,5 +55,54 @@ function filtrerTableau(tableau, predicate) {
     return tableau.filter(predicate);
 }
 // tester la fonction
-var tableauFiltre = filtrerTableau([5, 9, 1, 2, 4, 7, 3, 8], function (x) { return x % 2 === 0; });
-console.log(tableauFiltre);
+const tableauFiltre = filtrerTableau([5, 9, 1, 2, 4, 7, 3, 8], (x) => x % 2 === 0);
+// console.log(tableauFiltre);
+/**
+ * Ex06
+ */
+class Empileur {
+    constructor() {
+        this.pile = [];
+    }
+    empiler(element) {
+        this.pile.push(element);
+    }
+    depiler() {
+        return this.pile.pop();
+    }
+}
+// tester la classe Empileur
+// instancier la classe Empileur
+const empileurEntiers = new Empileur();
+empileurEntiers.empiler(8);
+empileurEntiers.empiler(4);
+empileurEntiers.empiler(7);
+// console.log(empileurEntiers);
+const sommet = empileurEntiers.depiler();
+// console.log(sommet);
+/**
+ * Ex07
+ * {"un": 1, "deux": 2}
+ */
+class Dictionnaire {
+    constructor() {
+        this.entries = new Map();
+    }
+    ajouter(key, value) {
+        this.entries.set(key, value);
+    }
+    recuperer(key) {
+        return this.entries.get(key);
+    }
+    supprimer(key) {
+        this.entries.delete(key);
+    }
+}
+// tester la classe Dictionnaire
+const dic1 = new Dictionnaire();
+dic1.ajouter("un", 1);
+dic1.ajouter("deux", 2);
+dic1.ajouter("trois", 3);
+console.log(dic1);
+const valDeux = dic1.recuperer("deux");
+console.log(valDeux);
